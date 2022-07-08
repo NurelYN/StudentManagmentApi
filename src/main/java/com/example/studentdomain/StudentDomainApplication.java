e4baf42982e52c8a08b4a8a5c20173226b3ce6ef
Nurel Nazmi <nurel.nazmi@brainstarstech.com>
2022-07-08 13:59:29 +0300

    First commit

---
+++ src/main/java/com/example/studentdomain/StudentDomainApplication.java
@@ -0,0 +1,13 @@
+package com.example.studentdomain;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudentDomainApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StudentDomainApplication.class, args);
+    }
+
+}
